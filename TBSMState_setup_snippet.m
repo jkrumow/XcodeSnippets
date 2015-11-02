fefda79daa316fd6953993a27d555dbdee718e06
Julian Krumow <julian.krumow@artcom.de>
2015-11-02 12:08:58 +0100

    Add setup snippet for TBSMState.

---
+++ TBSMState_setup_snippet.m
@@ -0,0 +1,8 @@
+---
+title: "TBSMStateSetup"
+summary: "Creates a TBSMState with name."
+completion-scope: All
+completion-prefix: "TBSMState_setup_snippet"
+---
+
+ TBSMState *<# state #> = [TBSMState stateWithName:@"<# name #>"];
