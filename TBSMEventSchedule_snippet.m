498278d91f388b576721e3d08e6fc06b66e92fe6
Julian Krumow <julian.krumow@tarbrain.com>
2016-02-05 23:24:28 +0100

    Add snippet for event scheduling.

---
+++ TBSMEventSchedule_snippet.m
@@ -0,0 +1,8 @@
+---
+title: "TBSMEvent schedule"
+summary: "Schedules a TBSMEvent with name and data."
+completion-prefix: "TBSMEventSchedule_snippet"
+completion-scope: CodeBlock
+---
+
+[<# stateMachine #> scheduleEventNamed:@"<# name #>" data:<# data #>];
